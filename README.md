b3f1a18d8e53385a9598f7a0ca496d30da8ecfcc
YuseiAbe <yabe@diverta.co.jp>
2021-05-18 15:22:02 +0900

    feat: make npm package && add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# enforcing-convensional-commit
+
+> a minimum tool for enforcing your repository to hook conventional commits at git commit.
+
+## useage
+
+`npm i enforcing-convensional-commit -D`
+
+You can enforce developpers to install the git-hook script if you follow the above way at executing general script `npm install`.
+
+Note that if user choose to install a single dependency,  
+the auto-process will not work.
+
+Alternatively,  
+you can install the git-hook script by just running sh script directly from web:  
+`source < (curl -s https://raw.githubusercontent.com/yabe-diverta/enforcing-convensional-commit/main/install)`
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-#!/usr/bin/env node
-
 const path = require('path');
 const { execSync } = require('child_process');
 
-const installScript = path.resolve(__dirname, 'install');
-
-try {
-    execSync(installScript, {stdio: 'inherit'});
-    console.info(`
-        The custom git hook is installed to your repository.
-        See ./.git/hooks/commit-msg
-    `);
-} catch(e) {
-    console.error(e);
+module.exports = () => {
+    const installScript = path.resolve(__dirname, 'install');
+    
+    try {
+        execSync(installScript, {stdio: 'inherit'});
+        console.info(`
+            The custom git hook is installed to your repository.
+            See ./.git/hooks/commit-msg
+        `);
+    } catch(e) {
+        console.error(e);
+    }
 }
